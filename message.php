d606c2aa9f0e28949f2b4771b244431c26cad5c0
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-03-22 22:28:26 +0100

    Update message.php
    Added all the messages

--- message.php
+++ message.php
@@ -1,14 +1,40 @@
 <?php
-    foreach ($_GET as $alert => $value) {
-        echo "<tr>";
-        echo "<td>";
-        echo $alert;
-        echo "</td>";
-        echo "<td>";
-        echo $value;
-        echo "</td>";
-        echo "</tr>";
+
+    $alert = (isset($_GET["alert"]))? $_GET["alert"]: "default";
+
+    switch($alert){
+        case "no-email" :
+            echo '<div class="alert alert-primary mt-5 w-50 mx-auto" role="alert">
+            Uw e-mailadres is onjuist of niet ingevuld!
+          </div>';
+          header("Refresh: 3; ./index.php?content=register");
+        break;
+        case "emailexists" :
+            echo '<div class="alert alert-primary mt-5 w-50 mx-auto" role="alert">
+            Het ingevulde e-mailadres bestaat al!
+          </div>';
+          header("Refresh: 3; ./index.php?content=register");
+        break;
+        case "registererror" :
+            echo '<div class="alert alert-primary mt-5 w-50 mx-auto" role="alert">
+            Er is een fout opgetreden, probeer het later nogmaals. Al doet dit probleem zich vaker voor, contacteer dan een medeweker.
+          </div>';
+          header("Refresh: 3; ./index.php?content=register");
+        break;
+        case "register-success" :
+            echo '<div class="alert alert-primary mt-5 w-50 mx-auto" role="alert">
+            Uw account is aangemaakt, controleer uw inbox.
+          </div>';
+          header("Refresh: 3; ./index.php?content=login");
+        break;
+        case "hacker-alert" :
+          echo '<div class="alert alert-primary mt-5 w-50 mx-auto" role="alert">
+          U heeft geen rechten op deze pagina.
+        </div>';
+        header("Refresh: 3; ./index.php?content=register");
+      break;
+        default;
+            header("Location: ./index.php?content=home");
+    break;
     }
-
-
 ?>
