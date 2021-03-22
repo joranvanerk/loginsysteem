a0268fa8651948872e290bc6610b4bf6a2f10cc5
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-03-22 22:27:53 +0100

    Create connect_db.php
    Added database file

---
+++ connect_db.php
@@ -0,0 +1,10 @@
+<?php 
+
+define("SERVERNAME", "localhost");
+define("USERNAME", "user");
+define("PASSWORD", "77yuh1vKrljABsgq");
+define("DBNAME", "audio-master");
+
+$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+?>
