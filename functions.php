5d1800c8364a93fd6f2545a158f2bc7e434266d0
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-04-20 08:35:10 +0200

    Update functions.php
    added the hash function

--- functions.php
+++ functions.php
@@ -1,10 +1,26 @@
 <?php
 
+include("./connect_db.php");
+
 function sanitize($raw_data) {
     global $conn;
     $data = htmlspecialchars($raw_data);
     $data = mysqli_real_escape_string($conn, $data);
     return $data;
+}
+
+function am_password_hash(){
+
+    $t = time();
+
+    $password = $t/60;
+
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    
+    return array("password_hash" => $password_hash,
+                "time" => $t,
+                "t60" => $password);
+
 }
 
 
