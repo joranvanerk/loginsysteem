e12405a132e3d58a4a3c26422824be063e1b1ca2
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-04-20 08:35:02 +0200

    Update activate_script.php
    updated activate scripts

--- activate_script.php
+++ activate_script.php
@@ -12,11 +12,35 @@ if (empty($_POST["password"]) || empty($_POST["passwordCheck"])){
 }elseif (strcmp($password, $passwordCheck)){
     header("Location: ./index.php?content=message&alert=password-notsame&id=$id&pwh=$pwh");
 }else {
-    $sql = "SELECT * FROM `register` WHERE `id` = $id AND `password` = '$pwh'";
+    $sql = "SELECT * FROM `register` WHERE `id` = $id";
 
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result)){
+
+        $record = mysqli_fetch_assoc($result);
+
+        if ($record["activated"]){
+            header("location: ./index.php?content=message&alert=already-active");
+        }else {
+
+            if (!strcmp($record["password"], $pwh)){
+                $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        
+                $updatesql = "UPDATE `register` SET `password`='$password_hash', `password` = 1 WHERE `register`.`id`=$id AND `password`='$pwh'";
+        
+                if(mysqli_query($conn, $updatesql)){
+                    header("location: ./index.php?content=message&alert=updatesuccess");
+                }else {
+                    header("location: ./index.php?content=message&alert=updatesuccess&id=$id&pwh=$pwh");
+                }
+            }else {
+                header("Location: ./index.php?content=message&alert=no-match-pwh");
+            }
+            
+        }
+
+
 
     }else {
         header("Location: ./index.php?content=message&alert=nopwhm");
